a4eef5b5b3d960998b54e0067cd46919d8320765
visualstudio <visualstudio@122-PC18>
2019-01-18 21:07:43 -0500

    Commit1

--- C0727100/Program.cs
+++ C0727100/Program.cs
@@ -10,6 +10,33 @@ namespace C0727100
     {
         static void Main(string[] args)
         {
+            {
+                StudentInformation _Mary = new StudentInformation("Mary", 3.8, 20);
+                StudentInformation _john = new StudentInformation("john", 3.8, 20);
+                StudentInformation _mark = new StudentInformation("mark", 3.8, 20);
+                StudentInformation _steve = new StudentInformation("steve", 3.8, 20);
+                StudentInformation _cindy = new StudentInformation("cindy", 3.8, 20);
+                Console.ReadLine();
+            }
         }
+
+
+
+
+        class StudentInformation
+        {
+            public string Name;
+            public double GPA;
+            public int Attendence;
+            public StudentInformation(string aName, double cGPA, int dAttendence)
+
+            {
+                Name = aName;
+
+                GPA = cGPA;
+                Attendence = dAttendence;
+            }
+        }
+        
     }
 }
