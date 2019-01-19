ae89644cab1839abc0ef50d44d063e6bafd2afbe
visualstudio <visualstudio@122-PC18>
2019-01-18 21:40:53 -0500

    Final code for handin

--- C0727100/Program.cs
+++ C0727100/Program.cs
@@ -16,6 +16,16 @@ namespace C0727100
                 StudentInformation _mark = new StudentInformation("mark", 3.8, 20);
                 StudentInformation _steve = new StudentInformation("steve", 3.8, 20);
                 StudentInformation _cindy = new StudentInformation("cindy", 3.8, 20);
+                var studentlist = new List<StudentInformation>();
+                studentlist.Add(_Mary);
+                studentlist.Add(_john);
+                studentlist.Add(_mark);
+                studentlist.Add(_steve);
+                studentlist.Add(_cindy);
+                foreach (var student in studentlist)
+                {
+                    Console.WriteLine(student.Name);
+                }
                 Console.ReadLine();
             }
         }
@@ -24,6 +34,7 @@ namespace C0727100
 
 
         class StudentInformation
+         
         {
             public string Name;
             public double GPA;
